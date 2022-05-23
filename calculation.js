1d20fa5db97dfe287b61fa8f5d760e6901416274
kiranpatils <patilkiran.101@gmail.com>
2022-05-23 12:20:29 +0530

    added text

---
+++ calculation.js
@@ -0,0 +1,10 @@
+var a = parseInt(process.argv[2]);
+var b = process.argv[3];
+var c = parseInt(process.argv[4]);
+
+if(b == '+')
+{
+    var answer1 = a + c;
+}
+
+console.log("your ans " + answer1);
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log('First npm package');
+console.log('First npm package 123');
 
 var chalk = require('chalk');
 console.log(chalk.green('I am a green line '  + chalk.blue.underline.bold(' with a blue substring ')  + 'that become green'));
+
+console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+
