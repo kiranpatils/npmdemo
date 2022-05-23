13aa28034ec7bdb7743cdc891e046b4f89742e8e
Sagar V <sagarv@leotechnosoft.net>
2022-05-23 15:21:16 +0530

    created conflicts in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('First npm package 123');
 
 var chalk = require('chalk');
+console.log('this is sagar's code')
 console.log(chalk.green('I am a green line here 123 '  + chalk.blue.underline.bold(' with a blue substring ')  + 'that become green'));
 
 console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
