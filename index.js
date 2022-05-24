eff914fb10a1730a809f4f1a64db7cb4ad09e997
kiranpatils <patilkiran.101@gmail.com>
2022-05-24 17:54:02 +0530

    my comments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('First npm package 123');
 var chalk = require('chalk');
-console.log(chalk.green('I am a green line here 123456 '  + chalk.blue.underline.bold(' with a blue substring ')  + 'that become green'));
+console.log(chalk.green('I am a green line here 123456 new message '  + chalk.blue.underline.bold(' with a blue substring ')  + 'that become green'));
 
 console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
 
